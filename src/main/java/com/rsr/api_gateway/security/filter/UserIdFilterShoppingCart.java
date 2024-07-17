b50b1a3735a5c442aa8bd7d98c96e6c89b47a3be
Grischakrombacha <grischa.storch@gmail.com>
2024-07-17 06:51:10 +0200

    Added ShoppingCart Connection

--- src/main/java/com/rsr/api_gateway/security/filter/UserIdFilterShoppingCart.java
+++ src/main/java/com/rsr/api_gateway/security/filter/UserIdFilterShoppingCart.java
@@ -1,43 +1,48 @@
 package com.rsr.api_gateway.security.filter;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
-@Slf4j
-@Order(0)
 public class UserIdFilterShoppingCart extends AbstractGatewayFilterFactory<UserIdFilterShoppingCart.Config> {
 
     public UserIdFilterShoppingCart() {
         super(Config.class);
     }
 
+    public static class Config {
+        // Configuration properties for your filter
+    }
+
     @Override
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            if (authentication != null && authentication.getPrincipal() instanceof Jwt) {
-                Jwt jwt = (Jwt) authentication.getPrincipal();
-                String userId = jwt.getClaimAsString("sub");
-                log.info("Filter wird durchlaufen");
-                // Add the userId to the path
-                ServerHttpRequest request = exchange.getRequest().mutate()
-                        .path(exchange.getRequest().getPath().toString().replace("/shopping-cart", "/shopping-cart/" + userId))
-                        .build();
-                return chain.filter(exchange.mutate().request(request).build());
-            }
-            return chain.filter(exchange);
+            return ReactiveSecurityContextHolder.getContext()
+                    .map(context -> context.getAuthentication())
+                    .flatMap(authentication -> {
+                        if (authentication != null && authentication.isAuthenticated()) {
+                            String userId = getUserIdFromAuthentication(authentication);
+                            // Hier sicherstellen, dass der Pfad korrekt zusammengesetzt wird
+                            String newPath = exchange.getRequest().getURI().getPath() + "/" + userId;
+                            System.out.println("New Path: " + newPath); // Debug-Log
+                            ServerHttpRequest request = exchange.getRequest().mutate().path(newPath).build();
+                            ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
+                            return chain.filter(mutatedExchange);
+                        }
+                        return chain.filter(exchange);
+                    }).then(Mono.fromRunnable(() -> System.out.println("Filter completed"))); // Debug-Log
         };
     }
 
-    public static class Config {
-        // Put configuration properties here if needed
+    private String getUserIdFromAuthentication(Authentication authentication) {
+        // Assuming the user ID is stored in the "sub" claim of the JWT token
+        return authentication.getName();
     }
 }
